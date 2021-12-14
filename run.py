7ae923764136f6f8a84e58626357d84f58f11979
Javier <notri80@gmail.com>
2021-12-14 13:20:18 +0100

    Validate survey data entered

--- run.py
+++ run.py
@@ -14,55 +14,61 @@ SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
 SHEET = GSPREAD_CLIENT.open("Gaming_survey")
 
+current_user_answer = []
 
 def get_answers_survey():
-    
-    
+    """ 
+    Access to the survey worksheet
+    Show questions to the user and get user's answers
+    """
     print("Hey Captain, great to have to someone to beat to!\n")
     print("Before beating you, I'd like you to participate in a quick gaming survey.\n")
     start_survey = input("Ready to answer 4 simple questions? Y/N\n").upper()
      
     survey_data = SHEET.worksheet("survey")
     questions = survey_data.get_all_values()
-
+    
+    global current_user_answer
     if start_survey == "Y":
 
         while True:
             for question in questions:
                 print(f"{question[0]}")
                 answer_1 = input().lower()
-                validate_data(answer_1, question)
-    
-                print(f"{question[1]}")
-                print("every week | twice a month | once in a while")
+                if not validate_data(answer_1, question[0]):
+                    break
+                print(f"{question[1]}\n every week | twice a month | once in a while")
                 answer_2 = input().lower()
-                validate_data(answer_2, question[1])
-    
-                print(f"{question[2]}")
-                print("computer | video console | both alike")
+                if not validate_data(answer_2, question[1]):
+                    break
+                print(f"{question[2]}\n computer | video console | both alike")
                 answer_3 = input().lower()
-                validate_data(answer_3)
-    
-                print(f"{question[3]}")
-                print("strategy | shooting | sports")
+                if not validate_data(answer_3, question[2]):
+                    break   
+                print(f"{question[3]}\n strategy | shooting | sports")
                 answer_4 = input().lower()
-                validate_data(answer_4)
-    
-                time.sleep(2)
+                if not validate_data(answer_4, question[3]):
+                    break
+                # elif validate_data(answer_4, question[3]):  
+                print(current_user_answer)
+
+                time.sleep(2)                
                 print("LET THE BATTLE BEGIN")
                 time.sleep(2)
-
+                exit()
 
 def validate_data(answer, question):
     choises = ["male", "female", "every week", "twice a month", " once in a while", "computer", "video console", "both alike", "strategy", "shooting", "sports"]
     try:
-        if answer is not choises:
+        current_user_answer.append(answer)
+        if answer not in choises:
+            current_user_answer.clear()
+            print(current_user_answer)
             raise ValueError(
                 "sorry! you must select one of the given options.\n")
             
     except ValueError as e:
         print(f"Invalid answer {e}, Please try again.")
-        print(f"{question}")
         return False
 
     return True
@@ -80,3 +86,4 @@ get_answers_survey()
 #         self.c = c
 
 # question_1 = Question()
+
