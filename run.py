742eb4ffc48a8c1d3e2109982d15a9205e07c464
Javier <notri80@gmail.com>
2021-12-14 23:40:01 +0100

    Calculate data gender percentages

--- run.py
+++ run.py
@@ -41,7 +41,6 @@ def get_answers_survey():
     Access to the survey worksheet
     Show questions to the user and get user's answers
     """
-    
     survey_data = SHEET.worksheet("survey")
     questions = survey_data.get_all_values()
     
@@ -74,7 +73,10 @@ def get_answers_survey():
 
         
 def validate_data(answer):
-
+    """ 
+    Inside the try clear the user's answers list and
+    raises an error if incorrect data entered
+    """
     choises = ["male", "female", "weekly", "twice a month", " once in a while", "computer", "video console", "both alike", "strategy", "shooting", "sports"]
     try:
         if answer not in choises:
@@ -91,47 +93,66 @@ def validate_data(answer):
 
 
 def add_survey_data_worksheet(user_answers, worksheet):
-    
+    """
+    Access the survey worksheet
+    Append the user's data into the worksheet
+    """
     data_worksheet = SHEET.worksheet(worksheet)
     new_data = data_worksheet.append_row(user_answers)
 
 
 def get_data_gender_worksheet(worksheet):
-
+    """"
+    Get user's gender data from worksheet
+    Calculate data percetages
+    """
     data_gender = worksheet.col_values(1)
-    male_gender = data_gender.count("male")
-    female_gender = data_gender.count("female")
-    # people = len(gender) - 1
-    print(f"male: {male_gender}, female: {female_gender}")
+    male_data = data_gender.count("male")
+    female_data = data_gender.count("female")
+    people = len(data_gender) - 1
+    # print(f"male: {male_gender}, female: {female_gender}")
+    total_male = round(int((male_data / people) * 100))
+    total_female = round(int((female_data / people) * 100))
+    print(f"male: {total_male}%, female: {total_female}%")
+    
+    return total_male, total_female 
 
 
 def get_data_disponibility_worksheet(worksheet):
-
+    """ 
+    Get data user's disponibility to play video games from worksheet
+    Calculate data percentages
+    """
     data_disponibility = worksheet.col_values(2)
     weekly = data_disponibility.count("weekly")
     twice_week = data_disponibility.count("twice a week")
     once_in_while = data_disponibility.count("once in a while")
-    print(F"weekly: {weekly}, twice a week: {twice_week}, once in a while: {once_in_while}")
-
+    # print(F"weekly: {weekly}, twice a week: {twice_week}, once in a while: {once_in_while}")
+    return weekly, twice_week, once_in_while
 
 def get_data_devices_worksheet(worksheet):
-
+    """"
+    Get data user's prefered devices from worksheet
+    Calculate data percentages
+    """
     data_devices = worksheet.col_values(3)
     computer = data_devices.count("computer")
     video_console = data_devices.count("video console")
     both_devices = data_devices.count("both alike")
-    print(F"computer: {computer}, video console: {video_console}, both devices: {both_devices}")
-
+    # print(F"computer: {computer}, video console: {video_console}, both devices: {both_devices}")
+    return computer, video_console, both_devices
 
 def get_data_game_plot(worksheet):
-
+    """"
+    Get data user's prefered kind of game from worksheet
+    Calculate data percentages
+    """
     data_game_plot = worksheet.col_values(4)
     strategy = data_game_plot.count("strategy")
     shooting = data_game_plot.count("shooting")
     sports = data_game_plot.count("sports")
-    print(f"strategy: {strategy}, shooting: {shooting}, sports: {sports}")
-
-
+    # print(f"strategy: {strategy}, shooting: {shooting}, sports: {sports}")
+    return strategy, shooting, sports
 
 
 def main():
@@ -139,10 +160,11 @@ def main():
     data_worksheet = SHEET.worksheet("survey")
     # user_answers = get_answers_survey()
     # add_survey_data_worksheet(user_answers, "survey")
-    get_data_gender_worksheet(data_worksheet)
-    get_data_disponibility_worksheet(data_worksheet)
-    get_data_devices_worksheet(data_worksheet)
-    get_data_game_plot(data_worksheet)
+    data_gender = get_data_gender_worksheet(data_worksheet)
+    data_disponibility = get_data_disponibility_worksheet(data_worksheet)
+    data_devices = get_data_devices_worksheet(data_worksheet)
+    data_game_plot = get_data_game_plot(data_worksheet)
+    # calculate_data = calculate_data_survey(data_gender, data_disponibility, data_devices, data_game_plot)
 
 # start_survey()
 main()
