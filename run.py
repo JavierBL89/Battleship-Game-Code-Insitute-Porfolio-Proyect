e0ac1a2f0a5699305947f458111f0b9c8b1fd534
Javier <notri80@gmail.com>
2021-12-29 00:11:12 +0100

    Developing UX part 1

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import random
 import time
-from random import randint
-from pprint import pprint
+import random
+from typing import Sized
 
 
 class Board:
@@ -20,13 +20,12 @@ class Board:
         self.player_shots = []
         self.computer_shots = []
         self.player_ships = [(7, 5), (4, 5), (0, 3)]
-        self.computer_ships = [(7, 5), (6, 5), (0, 3), (2, 0), (7, 
-2)]
+        self.computer_ships = [(7, 5), (6, 5), (0, 3), (2, 0), (7, 2)]
                
 
     def column_cordenates():
         translate_cordenates = {
-            "a" : 0, "b": 1, "c": 2, "d": 3, "f": 4, "g": 5, "h": 6, "i": 7
+            "a": 0, "b": 1, "c": 2, "d": 3, "f": 4, "g": 5, "h": 6, "i": 7
             }
         return translate_cordenates
 
@@ -38,7 +37,7 @@ class Board:
         total_ships = 1
         while total_ships <= 5:
             x = random.randint(0, size) - 1
-            y = random.randint(0, size)- 1
+            y = random.randint(0, size) - 1
             if type == "computer":
                 self.computer_ships.append((x, y))
             elif type == "player":
@@ -54,12 +53,12 @@ class Board:
         try:
             print("\nYou shot!")
             guess_row = int(input("\nRow: ")) - 1
-            while guess_row not in [0,1,2,3,4,5,6,7]:
+            while guess_row not in [0, 1, 2, 3, 4, 5, 6, 7]:
                 print("Please enter  valid data, only whole numbers from 1 to 8 are valid!\n")
                 guess_row = int(input("Row: ")) - 1
 
             guess_column = int(input("Column: ")) - 1
-            while guess_column not in [0,1,2,3,4,5,6,7]:
+            while guess_column not in [0, 1, 2, 3, 4, 5, 6, 7]:
                 print("Please enter  valid data, only whole numbers from 1 to 8 are valid!\n")
                 guess_column = int(input("Column: ")) - 1
             
@@ -74,8 +73,8 @@ class Board:
         Creates computer random shots
         Updates the players battleship accordingly
         """
-        self.x = random.randint(0,7)
-        self.y = random.randint(0,7)
+        self.x = random.randint(0, 7)
+        self.y = random.randint(0, 7)
         # self.x = 7
         # self.y = 5
         compu_guess = self.x, self.y
@@ -146,43 +145,97 @@ class Board:
             print("Got you!")
         elif shooter == "computer" and shot not in self.player_ships:
             print("\nMissed!...\n")
-
         return count
-
+    
 
 def game_over(count):
     if count == 1:
         return False
     
-    
+
 def play_game(computer, player, size):
     """
     Start up game and run till the end of it
     """
+    # print("\nOhh! Long time waiting for this!")
+    # time.sleep(1)
+    # print("\nBefore get started, let me ask you...\n")
+    # time.sleep(1)
+    # shirt = input("\nWhat color is your ugly shirt\n?")
+    # time.sleep(1)
+    # print("\nahgg!! Really ugly")
+    # time.sleep(1)
+    # food = input("\n...and what did you last have for food?\n")
+    # player_name = shirt + food
+    # player_name = player_name.capitalize()
+    # print("\nI always like to know my adversary's name")
+    # time.sleep(1)
+    # print(f"\nI will put you to the ground {player_name}!")
+    # time.sleep(2)
+    # print("\nCheck this out for now...")
+    # time.sleep(2)
+    # print("\n----------------------------")
+    # print("          Players           ")
+    # print(f" Computer         {player_name}\n")
+    # print("       Number of ships      ")
+    # print("    5                  5\n  ")
+    # print("           Score            ")
+    # print("    0                  0    ")
+    # print("----------------------------")
+    # time.sleep(4)
+    # print("\n   +++++++++++++++++++")
+    # print("          LEYEND        ")
+    # print("    S = boats           ")
+    # print("    X = water           ")
+    # print("    $ = hit/shunk       ")
+    # print("   ++++++++++++++++++++")
+    # time.sleep(3)
+    # print("\n...your game board")
+    # time.sleep(1)
+    # print(f"\n     {player_name}")
+    # print("  A B C D F G H I")
+    # player.populate_board("player", size)
+    # time.sleep(4)
+    # print(f"\n...Today's your lucky day {player_name}")
+    # time.sleep(1)
+    # print("\n....I will lend you a radar...")
+    # time.sleep(2)
+    # print("\n     Radar")
+    # print("  A B C D F G H I")
+    # computer.populate_board("computer", Sized)
+    # print("\n...and i will let you shot first!")
+    # time.sleep(1)
+    # print(f"\n...I will see you in abism {player_name}!")
+    # time.sleep(2)
     game = True
     while game:
+
         # computer.random_ship(size, "computer")
         # player.random_ship(size, "player")
         
         player_shot = player.player_guess()
         computer_shot = computer.computer_guess()
+        time.sleep(1)
+        count = player.validate_shot(player_shot, "player")
+        time.sleep(2)
+        print(f"\nMy shot is... {computer_shot}")
+        time.sleep(2)
+        count = computer.validate_shot(computer_shot, "computer")
+        time.sleep(1)
         computer.check_guess(player_shot)
-        print("\n  Computer Radar")
-        print("  A B C D F G H I")
-        computer.populate_board("computer", player_shot)
-        count = computer.validate_shot(player_shot, "player")
+        # print(f"     {player_name}")
+        print("  1 2 3 4 5 6 7 8")
+        player.populate_board("player", computer_shot)
         game_over(count)
         if game is False:
             print("""Well well well little bitch!!\n
 You win this time...\n
 I'll come back stronger and fuck your pretty ass!!\n""")
             break
-        print(f"\nMy shot is... {computer_shot}")
-        time.sleep(2)
-        print("\n   My Battleship")
-        print("  A B C D F G H I")
-        player.populate_board("player", computer_shot)
-        count = player.validate_shot(computer_shot, "computer")
+        time.sleep(1)
+        print("\n      Radar")
+        print("  1 2 3 4 5 6 7 8")
+        computer.populate_board("computer", player_shot)
         game_over(count)
         if game is False:
             print("I knew i could beat you!\n")
